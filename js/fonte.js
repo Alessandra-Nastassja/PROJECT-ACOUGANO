effc3394ac38009c61228ff16557ad620ebce64f
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2018-08-20 21:41:47 -0300

    Corrigindo erros do aumentar e diminuir fontes

--- js/fonte.js
+++ js/fonte.js
@@ -2,9 +2,14 @@ function fonte(e) {
 
 	var elemento = $(".acessibilidade");
 	var fonte = parseInt(elemento.css('font-size'));
-	
-	e == 'a' ? fonte++ : fonte--;
 
+	// e == 'a' ? fonte++ : fonte--;
+
+	if(e == 'a'){
+		fonte+= 3;
+	}else{
+		fonte-= 4;
+	}
 	elemento.css("fontSize", fonte);
-	
+
 }
